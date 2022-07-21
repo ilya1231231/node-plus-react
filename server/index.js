af95159336941943f3ede65db73e761c46961587
ilya <malygin.ilja@yandex.ru>
2022-07-21 20:07:01 +0300

    Написал обработчик событий и middleware

--- server/index.js
+++ server/index.js
@@ -4,15 +4,18 @@ const sequelize = require('./db')
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlerMiddleware')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
 //Подключаем роуты с бэкенда
 app.use('/api', router)
+
+//обработка ошибок, последний middleware
+app.use(errorHandler)
 
 const start = async () => {
     try {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,10 @@
 const Router = require('express')
+const userController = require('../controller/userController')
 const router = new Router()
 
-router.post('/registation')
-router.post('/login')
+router.post('/registation', userController.registration)
+router.post('/login',  userController.login)
 // Доступно по url /api/user/auth
-router.get('/auth', (req, res) => {
-    res.json({message: 'WORKING'})
-})
+router.get('/auth',  userController.check)
 
 module.exports = router
