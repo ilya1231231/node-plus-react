787ef847e5c60fffc8d7edff18b97916e8161527
ilya <malygin.ilja@yandex.ru>
2022-07-25 20:11:55 +0300

    Сделал апи для создания девайся с возможностью прикрепления файла

--- server/controller/deviceController.js
+++ server/controller/deviceController.js
@@ -1,9 +1,25 @@
-
+//генерирует неповторяемые ids
+const uuid = require('uuid')
+const path = require('path')
+const {Device} = require('../models/models')
+const ApiError = require('../error/ApiError') 
 
 class DeviceController {
     
     async create(req, res) {
-
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            //генерация названия файла
+            let fileName = uuid.v4() + ".jpg"
+            const rating = 3
+            //перемещаем изображение в папку static
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            const device = await Device.create({name, price, brandId, typeId, img: fileName, rating})
+            return res.json(device)
+        } catch (e) {
+            console.log(e.message)
+        }
     }
 
     async getAll(req, res) {
--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 require('dotenv').config()
-const models = require('./models/models')
+// const models = require('./models/models')
 const sequelize = require('./db')
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlerMiddleware')
 
 const PORT = process.env.PORT || 5000
 
+//регистрация приложений
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 //Подключаем роуты с бэкенда
 app.use('/api', router)
 
