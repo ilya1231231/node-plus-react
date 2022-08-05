e2db190b0af9159fa9537c6a10cb23cfa64afa4d
ilya <malygin.ilja@yandex.ru>
2022-08-05 20:50:39 +0300

    добавил роуты, почему-то не мапится с массива элементы

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
-    <div>
-      РАБОТАЕТ
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import {Routes, Route, Redirect} from "react-router-dom"
+import { authRoutes } from "../routes"
+import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE, LOGIN_ROUTE} from "../utils/consts"
+import Basket from "../pages/Basket"
+import Device from "../pages/Device"
+import Auth from "../pages/Auth"
+import Shop from "../pages/Shop"
+import Admin from "../pages/Admin"
+
+const AppRouter = () => {
+    const isAuth = false
+    return(
+        <Routes>
+            <Route exact path={DEVICE_ROUTE} element={<Device/>}/>
+            <Route exact path={REGISTRATION_ROUTE} element={<Auth/>}/>
+            <Route exact path={SHOP_ROUTE} element={<Shop/>}/>
+            <Route exact path={BASKET_ROUTE} element={<Basket/>}/>
+            <Route exact path={ADMIN_ROUTE} element={<Admin/>}/>
+            <Route exact path={LOGIN_ROUTE} element={<Auth/>}/>
+        </Routes>
+    )
+}
+
+export default AppRouter;
