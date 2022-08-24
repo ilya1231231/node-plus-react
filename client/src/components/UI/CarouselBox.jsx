5b4f518f93a45a563b9dc0a8edd9730a3df5ee5d
ilya <malygin.ilja@yandex.ru>
2022-08-24 19:06:23 +0300

    Добавил характеристики и карусельбокс для показа фотографий товара

---
+++ client/src/components/UI/CarouselBox.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+
+const CarouselBox = ({props}) => {
+
+    return(
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src="https://avatars.mds.yandex.net/get-mpic/6277643/img_id5167684583275161810.jpeg/orig"
+            alt={props.name}
+          />
+          <Carousel.Caption
+            border='3px sold red'
+            >
+            <h3>{props.name}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='https://items.s1.citilink.ru/1652708_v01_b.jpg'
+            alt={props.name}
+          />
+          <Carousel.Caption>
+            <h3>{props.name}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+}
+
+export default CarouselBox;
--- client/src/pages/Device.jsx
+++ client/src/pages/Device.jsx
@@ -1,7 +1,7 @@
 import React from "react"
-import { Container } from "react-bootstrap";
-import {Col, Image, Row} from 'react-bootstrap';
+import {Col, Image, Row, Card, Button, Container} from 'react-bootstrap';
 import bigStar from '../assets/bigStar.png';
+import CarouselBox from "../components/UI/CarouselBox";
 
 const Device = () => {
     const device = 
@@ -9,21 +9,29 @@ const Device = () => {
             id: 2, 
             name: 'Samsung Galaxy', 
             img: 'https://cdn.svyaznoy.ru/upload/iblock/a05/samsung_pd_sm_g991_bzvgser_900x1200_1.jpg/resize/483x483/hq/',
-            rating: 3
+            rating: 3,
+            price: 50000
         };
+    const descriptions = [
+        {id: 1, title: 'Оперативная память', description: '6гб'},
+        {id: 2, title: 'Вес', description: '200гр'},
+        {id: 3, title: 'Экран', description: 'OLED'},
+        {id: 4, title: 'Камера', description: '20 Мегапикселей'},
+        {id: 5, title: 'Память', description: '124гб'},
+    ]
     return(
         <Container>
-            <div className="d-flex">
-                <Col md={4} className="justify-content-center">
-                    <Image width="auto" height={300} src={device.img}/>
+            <div className="row d-flex align-items-center">
+                <Col md={4} xs={12} className="d-flex justify-content-center align-items-center p-3">
+                <CarouselBox props={device}/>
                 </Col>
-                <Col md={4}>
-                    <Row className="d-flex flex-column justify-content-center align-items-center">
+                <Col md={4} xs={12}>
+                    <div className="d-flex flex-column justify-content-center align-items-center p-3">
                         <div>
                             <h2>{device.name}</h2>
                         </div>
                         <div
-                            className="d-flex align-items-center justify-content-center"   
+                            className="d-flex align-items-center justify-content-center p-3"   
                             style={{
                                 background: `url(${bigStar}) no-repeat center center`,
                                 width: 240,
@@ -34,12 +42,31 @@ const Device = () => {
                         >
                             {device.rating}
                         </div>
-                    </Row>
+                    </div>
                 </Col>
-                <Col md={4}>
-                    Добавить в корзину
+                <Col md={4} xs={12}>
+                    <Card 
+                        className="d-flex flex-column justify-content-around align-items-center"
+                        style={{width: 300, height: 300, border: '3px solid light'}}
+                        >
+                        <h3
+                            style={{fontSize: 30}}
+                            >От: {device.price} рублей</h3>
+                        <Button
+                            variant={"outline-dark"}
+                            >Добавить в корзину
+                        </Button>
+                    </Card>
                 </Col>
             </div>
+            <Row className="p-3">
+                <h1>Характеристики</h1>
+                {descriptions.map((info, index) => 
+                    <Col key={info.id} md={12} mt-3 style={{background: index % 2 ? 'lightgray' : 'transparent'}}>
+                        <h5>{info.title} : {info.description}</h5>
+                    </Col>
+                )}
+            </Row>
         </Container>
     )
 }
